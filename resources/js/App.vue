87de758a6db5848df0626c8d50e00a84f839fecc
Felipe Silva <felipe.dacs@gmail.com>
2021-01-08 22:30:46 -0300

    add vue e vue-router
    THANKS: https://garywoodfine.com/building-a-vue-spa-with-laravel/

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Vue Home Page </h1>
+
+        <p>
+            <router-link :to="{ name: 'home' }">Home</router-link> |
+            <router-link :to="{ name: 'hello' }">Hello</router-link>
+        </p>
+
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,80 @@
-require('./bootstrap');
+require("./bootstrap");
+
+require("./bootstrap");
+
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+//Main pages
+import App from "./App.vue";
+import Home from "./views/Home.vue";
+import Hello from "./views/Hello.vue";
+
+const router = new VueRouter({
+    mode: "history",
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: Home
+        },
+        {
+            path: "/hello",
+            name: "hello",
+            component: Hello
+        }
+    ]
+});
+
+const app = new Vue({
+    el: "#app",
+    components: { App },
+    router
+});
+
+/*
+
+// import Vue from "vue";
+window.Vue = require("vue");
+
+Vue.component("hello", require("./components/Hello.vue").default);
+
+const app = new Vue({
+    el: "#app"
+    // components: { App },
+    // router,
+});
+*/
+/*
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+
+import App from './views/App'
+import About from './views/About'
+import Home from './views/Home'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About,
+        },
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
+*/
