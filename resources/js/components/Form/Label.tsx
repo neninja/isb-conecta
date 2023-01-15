53f910112f35b8bcd4153c3d44a9c532fff63cb4
Felipe Silva <felipe.dacs@gmail.com>
2023-01-15 16:16:33 -0300

    :new: reset

--- resources/js/components/Form/Label.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Label = styled.label`
-    font-size: 0.9rem;
-    font-weight: bold;
-    color: var(--gray-light);
-    margin-bottom: 0.2rem;
-`
