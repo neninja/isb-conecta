85cc5c69086f069d0321fe70d24fd8a83ab54bc6
Felipe Silva <felipe.dacs@gmail.com>
2023-01-16 01:58:53 -0300

    :100: corrige ci

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,51 @@
 import { defineConfig } from 'vite';
+import path from "path";
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [
-        laravel({
+export default defineConfig(({ command, mode }) => {
+    const ci = process.env.CI;
+
+    const aditionalPlugins = [];
+    if (!ci) {
+        aditionalPlugins.push(laravel({
             input: 'resources/js/app.tsx',
             refresh: true,
-        }),
-        react(),
-    ],
-    test: {
-        include: ["resources/**/*.spec.{ts,tsx,js,jsx}"],
-        watchExclude: [
-            "**/node_modules/**",
-            "**/dist/**",
-            "**/vendor/**",
-            "**/public/**",
-            "**/app/**",
-            "**/bootstrap/**",
-            "**/config/**",
-            "**/database/**",
-            "**/docker/**",
-            "**/domain/**",
-            "**/lang/**",
-            "**/php/**",
-            "**/routes/**",
-            "**/storage/**",
-            "**/tests/**",
+        }));
+    }
+
+    return {
+        plugins: [
+            aditionalPlugins,
+            react(),
         ],
-        globals: true,
-        environment: "jsdom",
-        open: false,
-    },
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./resources/js"),
+            },
+        },
+        test: {
+            include: ["resources/**/*.spec.{ts,tsx,js,jsx}"],
+            watchExclude: [
+                "**/node_modules/**",
+                "**/dist/**",
+                "**/vendor/**",
+                "**/public/**",
+                "**/app/**",
+                "**/bootstrap/**",
+                "**/config/**",
+                "**/database/**",
+                "**/docker/**",
+                "**/domain/**",
+                "**/lang/**",
+                "**/php/**",
+                "**/routes/**",
+                "**/storage/**",
+                "**/tests/**",
+            ],
+            globals: true,
+            environment: "jsdom",
+            open: false,
+        },
+    };
 });
